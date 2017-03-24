0c86568a3a2d2762fafb956eafd08d8659bd5641
Gabriel Hutchison <ghutchison2600@flpoly.org>
2017-03-24 15:32:20 -0400

    Updated buttons to match theme

--- server/routes.js
+++ server/routes.js
@@ -93,7 +93,7 @@ module.exports = function (app, passport, express, mysqlConnection, replace) {
                     if (assignee_id == -1) {
                         assignee_id = DEFAULT_ASSIGNEE;
                     }
-                    // console.log("ML chose ", assignee_id);
+                    console.log("ML chose ", assignee_id);
                     var query = "INSERT INTO tickets " +
                         "(client, title, description, category, " +
                         "assignee_id, open_status) VALUES (" +
