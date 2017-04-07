e261c5536bd6fb077838d58f908a87d2a3a0adb1
Gabriel Hutchison <ghutchison2600@flpoly.org>
2017-04-07 17:08:20 -0400

    Added template mail.js

---
+++ mail/mail.js
@@ -0,0 +1,27 @@
+const CONSTANTS_GO = "up here, too";
+
+module.exports = function (mysqlConnection) {
+    return {
+        /*
+         * Here's is where some data will go regarding each function for
+         * mailing. It would be useful to specify what the inputs/outputs are
+         * and what the function does with them.
+         */
+        funcName: function(data, done) {
+            // if an error is returned, send it with
+            // done(err, null);
+            // else, return data with
+            // done(null, data);
+        },
+        /*
+         * ...
+         */
+        funcName2: function(data1, data2, data3) {
+            // ...
+        },
+        /*
+         * ...
+         */
+        },
+    };
+}
