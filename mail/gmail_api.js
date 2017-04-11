c6d30777fbdfad8fb13c783b4f93b8ab8a1da574
Gabriel Hutchison <ghutchison2600@flpoly.org>
2017-04-11 19:13:41 -0400

    Imported Nodejs modules, implemented getUpdate

---
+++ mail/gmail_api.js
@@ -0,0 +1,108 @@
+// For the most part stolen from
+// https://developers.google.com/gmail/api/quickstart/nodejs
+
+const SECRET_FILE = './client_secret.json';
+
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+// Remember user's auth preference for next time
+var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-quickstart.json';
+
+module.exports = {
+    // load client secret from file
+    // return callback with auth
+    obtainAuth: function(callback) {
+        var _this = this;
+        fs.readFile(SECRET_FILE, function processClientSecrets(err, content) {
+                if (err) {
+                    console.log('Error loading client secret file: ', err);
+                    return;
+                }
+                // Authorize client, then load API
+                var secret = JSON.parse(content);
+                _this.authorize(secret, callback);
+            });
+    },
+
+    /**
+     * Create an OAuth2 client with the given credentials, and then execute the
+     * given callback function.
+     *
+     * @param {Object} credentials The authorization client credentials.
+     * @param {function} callback The callback to call with the authorized client.
+     */
+    authorize: function(credentials, callback) {
+        var clientSecret = credentials.installed.client_secret;
+        var clientId = credentials.installed.client_id;
+        var redirectUrl = credentials.installed.redirect_uris[0];
+        var auth = new googleAuth();
+        var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+        // Check if we have previously stored a token.
+        fs.readFile(TOKEN_PATH, function(err, token) {
+            if (err) {
+                getNewToken(oauth2Client, callback);
+            } else {
+                oauth2Client.credentials = JSON.parse(token);
+                callback(oauth2Client, google);
+            }
+        });
+    },
+
+    /**
+     * Get and store new token after prompting for user authorization, and then
+     * execute the given callback with the authorized OAuth2 client.
+     *
+     * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+     * @param {getEventsCallback} callback The callback to call with the authorized
+     *     client.
+     */
+    getNewToken: function(oauth2Client, callback) {
+        var authUrl = oauth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: SCOPES
+        });
+        console.log('Authorize this app by visiting this url: ', authUrl);
+        var rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        var _this = this;
+        rl.question('Enter the code from that page here: ', function(code) {
+            rl.close();
+            oauth2Client.getToken(code, function(err, token) {
+                if (err) {
+                    console.log('Error while trying to retrieve access token', err);
+                    return;
+                }
+                oauth2Client.credentials = token;
+                _this.storeToken(token);
+                callback(oauth2Client, google);
+            });
+        });
+    },
+
+    /**
+     * Store token to disk be used in later program executions.
+     *
+     * @param {Object} token The token to store to disk.
+     */
+    storeToken: function(token) {
+        try {
+            fs.mkdirSync(TOKEN_DIR);
+        } catch (err) {
+            if (err.code != 'EEXIST') {
+                throw err;
+            }
+        }
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+        console.log('Token stored to ' + TOKEN_PATH);
+    },
+
+};
--- mail/mail.js
+++ mail/mail.js
@@ -1,13 +1,14 @@
-const CONSTANTS_GO = "up here, too";
+var gmail_api = require('./gmail_api.js');
+var base64url = require('base64url');
 
 module.exports = function (mysqlConnection) {
     return {
         /*
          * Automatically load all changes and handle new messages or adding
          * new messages to tickets.
-         * Return the status (true for good, false otherwise).
+         * Return the error.
          */
-        handleUpdates: function() {
+        handleUpdates: function(done) {
             // get last load time from database
             // getUpdates()
                 // if successful, set last load time in database
@@ -17,7 +18,7 @@ module.exports = function (mysqlConnection) {
                 // TODO decide what else should be done for various kinds of
                 //     new messages
             console.log('TODO handleUpdates');
-        }
+        },
         /*
          * Given a unix timestamp (UTC?) return all emails recieved since that
          * time (passed to 'done' as 'messages').
@@ -33,13 +34,93 @@ module.exports = function (mysqlConnection) {
          *        ...
          *    ]
          */
-        getUpdates: function(from, done) {
+        getUpdates: function(since, done) {
+            var total = 0, loaded = 0, emails = [];
+            function addOneLoaded() {
+                if (++loaded >= total) {
+                    // all have been loaded!
+                    done(null, emails);
+                }
+            }
+            function forEachLoaded(email) {
+                // Email will be the raw json data.
+                // Convert it to a more useable format and return it to done
+                var emailData = {
+                    "from": "",
+                    "to": "",
+                    "title": "",
+                    "body": "",
+                    "time": 0, // unix timestamp
+                };
+                var bodyRaw = email.payload.body.data;
+                if (bodyRaw === undefined || bodyRaw === "") {
+                    addOneLoaded();
+                    return;
+                }
+                emailData.body = base64url.decode(bodyRaw);
+
+                for (var i = 0; i < email.payload.headers.length; i++) {
+                    var header = email.payload.headers[i];
+                    if (header.name == 'To') { // 'To'
+                        var v = header.value;
+                        v = v.split('<')[1];
+                        v = v.substring(0, v.length - 1);
+                        emailData.to = v;
+                    } else if (header.name == 'Subject') { // 'Thread-Topic'
+                        emailData.title = header.value;
+                    } else if (header.name == 'Date') {
+                        emailData.time = new Date(header.value).getTime();
+                        // It's too old! Yes, too old to begin the training.
+                        if (emailData.time < since) {
+                            addOneLoaded();
+                            return;
+                        }
+                    } else if (header.name == 'Return-Path') { // 'From'
+                        emailData.from = header.value.substring(1,
+                            header.value.length - 1);
+                    }
+                }
+                emails.push(emailData);
+
+                addOneLoaded();
+            }
+            // get authentication rights to use API
+            gmail_api.obtainAuth(function(auth, google) {
+                // specifically use gmail API
+                var gmail = google.gmail('v1');
+
+                // get an overview of each message (ids)
+                gmail.users.messages.list({
+                    auth: auth,
+                    userId: 'me',
+                }, function(err, response) {
+                    if (err) {
+                        console.error("There was an error obtaining messages:", err);
+                        return;
+                    }
+                    total = response.resultSizeEstimate;
+                    var id;
+                    for (var i = 0; i < response.messages.length; i++) {
+                        id = response.messages[i].id;
+                        gmail.users.messages.get({
+                            auth: auth,
+                            userId: 'me',
+                            id: id,
+                        }, function(err, response) {
+                            if (err) {
+                                console.error("Failed to load those particular messages:", err);
+                                return;
+                            }
+                            forEachLoaded(response);
+                        });
+                    }
+                });
+            });
             console.log('TODO getUpdates');
         },
         /*
          * Given a recipient, title, and full html body, send the messages
-         * using the Gmail API. Return the status (true for sent, err
-         * otherwise).
+         * using the Gmail API. Return the error.
          */
         sendMessage: function(to, title, body, done) {
             console.log('TODO sendMessage');
@@ -56,7 +137,7 @@ module.exports = function (mysqlConnection) {
         /*
          * Given data about a ticket, add it to the database and notify the
          * manager that a ticket has been recieved.
-         * Returns the status as true or false.
+         * Returns the error.
          * Note: 'to' may be null, meaning no manager is assigned yet.
          */
         recievedNewTicket: function(title, body, from, to, done) {
@@ -66,10 +147,22 @@ module.exports = function (mysqlConnection) {
          * Given data about a message, add it to the database, associate it with
          * a ticket, and notify (email) the manager that a reply has been made
          * (if the sender was not the manager).
-         * Return the status as true or false.
+         * Return the error.
          */
         recievedNewMessage: function(ticketID, title, body, from, done) {
             console.log('TODO recievedNewMessage');
         },
+        // TODO remove
+        test: function() {
+            this.getUpdates(1490385831001, function(err, messages) {
+                if (err) {
+                    console.log("Totally recieved an error, bro:", err);
+                    return;
+                }
+                console.log("Messages:\n", messages[0]);
+            });
+        },
     };
 }
+
+module.exports(null).test();
